fcbc9d3de8eed2773288df806a7b5446d9b3fb95
Jaap Joris Vens <jj@rtts.eu>
2016-08-10 16:12:10 +0200

    RC6

--- bps/settings.py
+++ bps/settings.py
@@ -144,7 +144,7 @@ LOGGING = {
          'level': 'INFO',
          'class': 'logging.handlers.SysLogHandler',
          'facility': 'local7',
-         'address': '/dev/log',
+#         'address': '/dev/log',
        },
     },
     'loggers': {
