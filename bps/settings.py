ad586352338de27df9d5fcc5dcbfa06f94d856c0
Jaap Joris Vens <jj@rtts.eu>
2020-02-07 11:20:09 +0100

    Make settings file optional and move remaining UvT-specific files to the uvt_user app

--- bps/settings.py
+++ bps/settings.py
@@ -2,58 +2,33 @@ import os, sys
 import logging
 import configparser
 from django.core.exceptions import ImproperlyConfigured
-from .utils import read
+from .utils import read, random_string
 
-HUMAN_FRIENDLY_CHARS = '234679ABCDEFGHJKLMNPRSTUVWXYZabcdefghijkmnpqrstuvwxyz'
-def random_string(length):
-    return ''.join(random.choice(HUMAN_FRIENDLY_CHARS) for x in range(length))
+CONFIG_FILE = '/etc/bps/config.ini'
+cfg = configparser.ConfigParser()
+if os.path.isfile(CONFIG_FILE):
+    try:
+        cfg.read(CONFIG_FILE)
+    except configparser.Error as e:
+        raise ImproperlyConfigured("Error parsing %s: %s" % (CONFIG_FILE, e.message))
 
-if os.path.isfile('/etc/bps/config.ini'):
-    CONFIG_FILE = '/etc/bps/config.ini'
-elif os.path.isfile('bps/config.ini'):
-    CONFIG_FILE = 'bps/config.ini'
-else:
-    raise ImproperlyConfigured('Could not locate the configuration file {/etc/,}bps/config.ini')
-
+KEY_FILE = cfg.get('locations', 'secret_key', fallback='')
 try:
-    cfg = configparser.ConfigParser()
-    cfg.read(CONFIG_FILE)
-    static_dir    = cfg.get('locations', 'static_dir')
-    uploads_dir   = cfg.get('locations', 'uploads_dir')
-    secret_key    = cfg.get('locations', 'secret_key')
-    db_engine     = cfg.get('database', 'engine')
-    db_name       = cfg.get('database', 'name')
-    db_host       = cfg.get('database', 'hostname')
-    db_user       = cfg.get('database', 'username')
-    db_pass       = cfg.get('database', 'password')
-    cas_server    = cfg.get('misc', 'cas_server', fallback='')
-    allowed_hosts = cfg.get('misc', 'allowed_hosts')
-    https_only    = cfg.get('misc', 'https_only') in ['true', 'True', 'on', 'yes']
-    debug         = cfg.get('misc', 'debug') in ['true', 'True', 'on', 'yes']
-except configparser.Error as e:
-    raise ImproperlyConfigured("Error parsing %s: %s" % (CONFIG_FILE, e.message))
-
-try:
-    SECRET_KEY = read(secret_key)
+    SECRET_KEY = read(KEY_FILE)
 except IOError:
-    if debug or 'test' in sys.argv:
-        logging.warning('Secret key not found. Using randomly generated key.')
-        SECRET_KEY = random_string(50)
-    else:
-        raise ImproperlyConfigured('Could not read secret key file specified in config file: "{}"'.format(secret_key))
+    logging.warning('Secret key not found. Using randomly generated key.')
+    SECRET_KEY = random_string(50)
 
-STATIC_ROOT = static_dir
-MEDIA_ROOT = uploads_dir
-CAS_SERVER_URL = cas_server
+STATIC_ROOT = cfg.get('locations', 'static_dir', fallback='/srv/bps/static')
+MEDIA_ROOT = cfg.get('locations', 'uploads_dir', fallback='/srv/bps/media')
+CAS_SERVER_URL = cfg.get('misc', 'cas_server', fallback='')
 CAS_RESPONSE_CALLBACKS = ['uvt_user.cas.callback']
-ALLOWED_HOSTS = [e.strip() for e in allowed_hosts.split(',')]
-DEBUG = debug
+ALLOWED_HOSTS = ['*']
+DEBUG = cfg.get('misc', 'debug', fallback='true') in ['true', 'True', 'on', 'yes']
 ROOT_URLCONF = 'bps.urls'
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 WSGI_APPLICATION = 'bps.wsgi.application'
-PACKAGE_DIR = os.path.dirname(__file__)
-TEMPLATE_DIRS = [os.path.join(PACKAGE_DIR, 'templates')]
 STATIC_URL = '/static/'
 MEDIA_URL = '/uploads/'
 LANGUAGE_CODE = 'en-us'
@@ -61,16 +36,10 @@ TIME_ZONE = 'Europe/Amsterdam'
 USE_I18N = False
 USE_L10N = False
 USE_TZ = True
-if https_only and not DEBUG:
+if not DEBUG:
     SECURE_SSL_REDIRECT   = True
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE    = True
-
-STATICFILES_DIRS = [
-    os.path.join(PACKAGE_DIR, 'static'),
-    '/usr/share/javascript',
-]
-
 
 INSTALLED_APPS = [
     'autodidact',
@@ -93,7 +62,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'cas.middleware.CASMiddleware',
 ]
 
 AUTHENTICATION_BACKENDS = [
@@ -105,7 +73,6 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'APP_DIRS': True,
-        'DIRS': TEMPLATE_DIRS,
         'OPTIONS': {
             'debug': DEBUG,
             'context_processors': [
@@ -140,11 +107,11 @@ LOGGING = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.' + db_engine,
-        'NAME': db_name,
-        'HOST': db_host,
-        'USER': db_user,
-        'PASSWORD': db_pass,
+        'ENGINE': 'django.db.backends.' + cfg.get('database', 'engine', fallback='postgresql'),
+        'NAME': cfg.get('database', 'name', fallback='bps'),
+        'HOST': cfg.get('database', 'hostname', fallback=None),
+        'USER': cfg.get('database', 'username', fallback='bps'),
+        'PASSWORD': cfg.get('database', 'password', fallback=None),
     }
 }
 
--- bps/utils.py
+++ bps/utils.py
@@ -1,3 +1,9 @@
+import random
+
 def read(file):
     with open(file) as f:
         return f.read()
+
+HUMAN_FRIENDLY_CHARS = '234679ABCDEFGHJKLMNPRSTUVWXYZabcdefghijkmnpqrstuvwxyz'
+def random_string(length):
+    return ''.join(random.choice(HUMAN_FRIENDLY_CHARS) for x in range(length))
